1f3ee547472b8aab3ca5ae59c271a08ef975363b
isnakode <247353247+isnakode@users.noreply.github.com>
2026-06-04 10:07:37 +0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+const getSiteUrl = () => {
+  // Kalau di Cloudflare, pake URL bawaan mereka
+  if (process.env.CF_PAGES_URL) return process.env.CF_PAGES_URL;
+  // Kalau di Vercel, pake URL bawaan mereka
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  // Kalau lagi develop atau custom domain sendiri, pake environment variable manual
+  return process.env.SITE_URL || 'http://localhost:4321';
+};
+
+// https://astro.build/config
+export default defineConfig({
+  site: getSiteUrl(),
+  vite: {
+    server: {
+      allowedHosts: [
+        "uncut-eloquence-skilled.ngrok-free.dev"
+      ]
+    },
+    plugins: [tailwindcss()]
+  }
+});
